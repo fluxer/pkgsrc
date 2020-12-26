62c895b2a8ea6bfe83ed8b63d8f3e6a78582a137
Ivailo Monev <xakepa10@gmail.com>
2020-12-26 16:22:21 +0000

    add vala option for libsecret
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- security/libsecret/options.mk
+++ security/libsecret/options.mk
@@ -1,15 +1,24 @@
 # $NetBSD: options.mk,v 1.1 2016/12/01 12:51:12 martin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsecret
-PKG_SUPPORTED_OPTIONS=	introspection
-PKG_SUGGESTED_OPTIONS=	introspection
+PKG_SUPPORTED_OPTIONS=	introspection vala
+PKG_SUGGESTED_OPTIONS=	introspection vala
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	introspection
+PLIST_VARS+=	introspection vala
 .if !empty(PKG_OPTIONS:Mintrospection)
 .include "../../devel/gobject-introspection/buildlink3.mk"
 PLIST.introspection=yes
+CONFIGURE_ARGS+=	--enable-introspection=yes
 .else
-CONFIGURE_ARGS+=--disable-introspection
+CONFIGURE_ARGS+=	--enable-introspection=no
+.endif
+
+.if !empty(PKG_OPTIONS:Mvala)
+.include "../../lang/vala/buildlink3.mk"
+PLIST.vala=yes
+CONFIGURE_ARGS+=	--enable-vala=yes
+.else
+CONFIGURE_ARGS+=	--enable-vala=no
 .endif
