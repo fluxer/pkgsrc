7492b248ec794867efd6c8779f7a1be33a433d3b
Ivailo Monev <xakepa10@gmail.com>
2020-12-27 23:28:19 +0000

    merge introspection and vala libsecret options
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- security/libsecret/options.mk
+++ security/libsecret/options.mk
@@ -1,24 +1,20 @@
 # $NetBSD: options.mk,v 1.1 2016/12/01 12:51:12 martin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsecret
-PKG_SUPPORTED_OPTIONS=	introspection vala
-PKG_SUGGESTED_OPTIONS=	introspection vala
+PKG_SUPPORTED_OPTIONS=	introspection
+PKG_SUGGESTED_OPTIONS=	introspection
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	introspection vala
+PLIST_VARS+=	introspection
+# introspection requires vala
 .if !empty(PKG_OPTIONS:Mintrospection)
+.include "../../lang/vala/buildlink3.mk"
 .include "../../devel/gobject-introspection/buildlink3.mk"
 PLIST.introspection=yes
 CONFIGURE_ARGS+=	--enable-introspection=yes
-.else
-CONFIGURE_ARGS+=	--enable-introspection=no
-.endif
-
-.if !empty(PKG_OPTIONS:Mvala)
-.include "../../lang/vala/buildlink3.mk"
-PLIST.vala=yes
 CONFIGURE_ARGS+=	--enable-vala=yes
 .else
+CONFIGURE_ARGS+=	--enable-introspection=no
 CONFIGURE_ARGS+=	--enable-vala=no
 .endif
